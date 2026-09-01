b0691541393aff421cf9d058c0c9718f20e65db3
Guillaume L. <guillaumel@alvie.fr>
2026-09-01 20:26:26 +0200

    fix: correct iOS example module name after upstream merge (TECH-2394)

--- example/ios/TiktokBusinessExample/AppDelegate.swift
+++ example/ios/TiktokBusinessExample/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "TikTokBussinessExample",
+      withModuleName: "TikTokBusinessExample",
       in: window,
       launchOptions: launchOptions
     )
